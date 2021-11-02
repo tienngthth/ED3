3a9ec32ac51f17c4b0f64f0481b42f9eb4485601
Tien Nguyen <tien.nguyen@skedulo.com>
2021-11-02 15:10:33 +0700

    Socket io server

---
+++ README.md
@@ -0,0 +1,2 @@
+1. run `npm install` to install all neccessary libraries
+2. run `node .\server.js` to start the server
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('image', function (data) {                     // listen on client emit 'data'
+      io.emit('image', data);                                 // emmit to socket
+    })
+
+    socket.on('result', function (data) {                    
+        io.emit('result', data);                                 
+      })
+  })
+  
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
